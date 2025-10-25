5c7a450ed2db74bd5f54911de3dfbfe07f98b8d6
LittleBlue7577 <matthew7577@gmail.com>
2025-10-25 13:10:35 +0800

    Original Files

--- BankDatabase.java
+++ BankDatabase.java
@@ -1,73 +1,26 @@
 // BankDatabase.java
 // Represents the bank account information database 
 
-public class BankDatabase {
+public class BankDatabase
+{
    private Account accounts[]; // array of Accounts
-
+   
    // no-argument BankDatabase constructor initializes accounts
-   public BankDatabase() {
-      accounts = new Account[50]; // Maximum 50 accounts
-      // Create some sample accounts (both saving and cheque accounts)
-      accounts[0] = new SavingAccount(12345, 54321, 1000.0, 1200.0);
-      accounts[1] = new ChequeAccount(98765, 56789, 200.0, 200.0);
-      accounts[2] = new SavingAccount(11451, 19198, 19000000000.0, 19000000001.0);
-      accounts[3] = new ChequeAccount(77777, 99999, 79999.0, 799999.0);
+   public BankDatabase()
+   {
+      accounts = new Account[ 2 ]; // just 2 accounts for testing
+      accounts[ 0 ] = new Account( 12345, 54321, 1000.0, 1200.0 );
+      accounts[ 1 ] = new Account( 98765, 56789, 200.0, 200.0 );  
    } // end no-argument BankDatabase constructor
-
-   // determine if the account is a saving account
-   public boolean isSavingAccount(int accountNumber) {
-      Account account = getAccount(accountNumber);
-      return account instanceof SavingAccount;
-   } // end method isSavingAccount
-
-   // determine if the account is a cheque account
-   public boolean isChequeAccount(int accountNumber) {
-      Account account = getAccount(accountNumber);
-      return account instanceof ChequeAccount;
-   } // end method isChequeAccount
-
-   // get interest rate for a saving account
-   public double getInterestRate(int accountNumber) {
-      Account account = getAccount(accountNumber);
-      if (account instanceof SavingAccount)
-         return ((SavingAccount) account).getInterestRate();
-      return 0.0; // return 0 if not a saving account
-   } // end method getInterestRate
-
-   // set interest rate for a saving account
-   public void setInterestRate(int accountNumber, double newRate) {
-      Account account = getAccount(accountNumber);
-      if (account instanceof SavingAccount)
-         ((SavingAccount) account).setInterestRate(newRate);
-   } // end method setInterestRate
-
-   // get limit per cheque for a cheque account
-   public double getLimitPerCheque(int accountNumber) {
-      Account account = getAccount(accountNumber);
-      if (account instanceof ChequeAccount)
-         return ((ChequeAccount) account).getLimitPerCheque();
-      return 0.0; // return 0 if not a cheque account
-   } // end method getLimitPerCheque
-
-   // set limit per cheque for a cheque account
-   public void setLimitPerCheque(int accountNumber, double newLimit) {
-      Account account = getAccount(accountNumber);
-      if (account instanceof ChequeAccount)
-         ((ChequeAccount) account).setLimitPerCheque(newLimit);
-   } // end method setLimitPerCheque
-
-   // check whether an account with specified account number exists
-   public boolean accountExists(int accountNumber) {
-      // attempt to retrieve the account with the account number
-      return getAccount(accountNumber) != null;
-   } // end method accountExists
-
+   
    // retrieve Account object containing specified account number
-   private Account getAccount(int accountNumber) {
+   private Account getAccount( int accountNumber )
+   {
       // loop through accounts searching for matching account number
-      for (Account currentAccount : accounts) {
+      for ( Account currentAccount : accounts )
+      {
          // return current account if match found
-         if (currentAccount.getAccountNumber() == accountNumber)
+         if ( currentAccount.getAccountNumber() == accountNumber )
             return currentAccount;
       } // end for
 
@@ -76,49 +29,56 @@ public class BankDatabase {
 
    // determine whether user-specified account number and PIN match
    // those of an account in the database
-   public boolean authenticateUser(int userAccountNumber, int userPIN) {
+   public boolean authenticateUser( int userAccountNumber, int userPIN )
+   {
       // attempt to retrieve the account with the account number
-      Account userAccount = getAccount(userAccountNumber);
+      Account userAccount = getAccount( userAccountNumber );
 
       // if account exists, return result of Account method validatePIN
-      if (userAccount != null)
-         return userAccount.validatePIN(userPIN);
+      if ( userAccount != null )
+         return userAccount.validatePIN( userPIN );
       else
          return false; // account number not found, so return false
    } // end method authenticateUser
 
    // return available balance of Account with specified account number
-   public double getAvailableBalance(int userAccountNumber) {
-      return getAccount(userAccountNumber).getAvailableBalance();
+   public double getAvailableBalance( int userAccountNumber )
+   {
+      return getAccount( userAccountNumber ).getAvailableBalance();
    } // end method getAvailableBalance
 
    // return total balance of Account with specified account number
-   public double getTotalBalance(int userAccountNumber) {
-      return getAccount(userAccountNumber).getTotalBalance();
+   public double getTotalBalance( int userAccountNumber )
+   {
+      return getAccount( userAccountNumber ).getTotalBalance();
    } // end method getTotalBalance
 
    // credit an amount to Account with specified account number
-   public void credit(int userAccountNumber, double amount) {
-      getAccount(userAccountNumber).credit(amount);
+   public void credit( int userAccountNumber, double amount )
+   {
+      getAccount( userAccountNumber ).credit( amount );
    } // end method credit
 
    // debit an amount from of Account with specified account number
-   public void debit(int userAccountNumber, double amount) {
-      getAccount(userAccountNumber).debit(amount);
+   public void debit( int userAccountNumber, double amount )
+   {
+      getAccount( userAccountNumber ).debit( amount );
    } // end method debit
 } // end class BankDatabase
 
+
+
 /**************************************************************************
- * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and *
- * Pearson Education, Inc. All Rights Reserved. *
- * *
- * DISCLAIMER: The authors and publisher of this book have used their *
- * best efforts in preparing the book. These efforts include the *
- * development, research, and testing of the theories and programs *
- * to determine their effectiveness. The authors and publisher make *
- * no warranty of any kind, expressed or implied, with regard to these *
+ * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
  * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or *
- * consequential damages in connection with, or arising out of, the *
- * furnishing, performance, or use of these programs. *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
  *************************************************************************/
--- Withdrawal.java
+++ Withdrawal.java
@@ -1,178 +1,150 @@
 // Withdrawal.java
 // Represents a withdrawal ATM transaction
 
-public class Withdrawal extends Transaction {
+public class Withdrawal extends Transaction
+{
    private int amount; // amount to withdraw
    private Keypad keypad; // reference to keypad
    private CashDispenser cashDispenser; // reference to cash dispenser
 
-   // constants for menu options
-   private final static int CANCELED = 7;
-   private final static int CUSTOM_AMOUNT = 6;
+   // constant corresponding to menu option to cancel
+   private final static int CANCELED = 6;
 
    // Withdrawal constructor
-   public Withdrawal(int userAccountNumber, Screen atmScreen,
-         BankDatabase atmBankDatabase, Keypad atmKeypad,
-         CashDispenser atmCashDispenser) {
+   public Withdrawal( int userAccountNumber, Screen atmScreen, 
+      BankDatabase atmBankDatabase, Keypad atmKeypad, 
+      CashDispenser atmCashDispenser )
+   {
       // initialize superclass variables
-      super(userAccountNumber, atmScreen, atmBankDatabase);
-
+      super( userAccountNumber, atmScreen, atmBankDatabase );
+      
       // initialize references to keypad and cash dispenser
       keypad = atmKeypad;
       cashDispenser = atmCashDispenser;
    } // end Withdrawal constructor
 
    // perform transaction
-   public void execute() {
+   public void execute()
+   {
       boolean cashDispensed = false; // cash was not dispensed yet
       double availableBalance; // amount available for withdrawal
 
       // get references to bank database and screen
-      BankDatabase bankDatabase = getBankDatabase();
+      BankDatabase bankDatabase = getBankDatabase(); 
       Screen screen = getScreen();
 
       // loop until cash is dispensed or the user cancels
-      do {
-         // obtain a chosen withdrawal amount from the user
+      do
+      {
+         // obtain a chosen withdrawal amount from the user 
          amount = displayMenuOfAmounts();
-
+         
          // check whether user chose a withdrawal amount or canceled
-         if (amount != CANCELED) {
+         if ( amount != CANCELED )
+         {
             // get available balance of account involved
-            availableBalance = bankDatabase.getAvailableBalance(getAccountNumber());
-
-            // check whether the user has enough money in the account
-            if (amount <= availableBalance) {
-               // check if this is a cheque account and amount exceeds limit
-               if (bankDatabase.isChequeAccount(getAccountNumber())) {
-                  double limit = bankDatabase.getLimitPerCheque(getAccountNumber());
-                  if (amount > limit) {
-                     screen.displayMessageLine(
-                           String.format("\nWithdrawal amount exceeds the limit of HK$%.2f for cheque accounts." +
-                                 "\n\nPlease choose a smaller amount.", limit));
-                     continue; // continue the loop to allow another attempt
-                  }
-               }
-
+            availableBalance = 
+               bankDatabase.getAvailableBalance( getAccountNumber() );
+      
+            // check whether the user has enough money in the account 
+            if ( amount <= availableBalance )
+            {   
                // check whether the cash dispenser has enough money
-               if (cashDispenser.isSufficientCashAvailable(amount)) {
+               if ( cashDispenser.isSufficientCashAvailable( amount ) )
+               {
                   // update the account involved to reflect withdrawal
-                  bankDatabase.debit(getAccountNumber(), amount);
-
-                  cashDispenser.dispenseCash(amount); // dispense cash
+                  bankDatabase.debit( getAccountNumber(), amount );
+                  
+                  cashDispenser.dispenseCash( amount ); // dispense cash
                   cashDispensed = true; // cash was dispensed
 
                   // instruct user to take cash
-                  screen.displayMessageLine(
-                        "\nPlease take your cash now.");
+                  screen.displayMessageLine( 
+                     "\nPlease take your cash now." );
                } // end if
                else // cash dispenser does not have enough cash
-                  screen.displayMessageLine(
-                        "\nInsufficient cash available in the ATM." +
-                              "\n\nPlease choose a smaller amount.");
+                  screen.displayMessageLine( 
+                     "\nInsufficient cash available in the ATM." +
+                     "\n\nPlease choose a smaller amount." );
             } // end if
             else // not enough money available in user's account
             {
-               screen.displayMessageLine(
-                     "\nInsufficient funds in your account." +
-                           "\n\nPlease choose a smaller amount.");
+               screen.displayMessageLine( 
+                  "\nInsufficient funds in your account." +
+                  "\n\nPlease choose a smaller amount." );
             } // end else
          } // end if
-         else // user chose cancel menu option
+         else // user chose cancel menu option 
          {
-            screen.displayMessageLine("\nCanceling transaction...");
+            screen.displayMessageLine( "\nCanceling transaction..." );
             return; // return to main menu because user canceled
          } // end else
-      } while (!cashDispensed);
+      } while ( !cashDispensed );
 
    } // end method execute
 
    // display a menu of withdrawal amounts and the option to cancel;
-   // return the chosen amount or CANCELED if the user chooses to cancel
-   private int displayMenuOfAmounts() {
+   // return the chosen amount or 0 if the user chooses to cancel
+   private int displayMenuOfAmounts()
+   {
       int userChoice = 0; // local variable to store return value
 
       Screen screen = getScreen(); // get screen reference
-
+      
       // array of amounts to correspond to menu numbers
-      int amounts[] = { 0, 200, 500, 1000, 2000, 5000 };
+      int amounts[] = { 0, 20, 40, 60, 100, 200 };
 
       // loop while no valid choice has been made
-      while (userChoice == 0) {
+      while ( userChoice == 0 )
+      {
          // display the menu
-         screen.displayMessageLine("\nWithdrawal Menu:");
-         screen.displayMessageLine("1 - $200");
-         screen.displayMessageLine("2 - $500");
-         screen.displayMessageLine("3 - $1,000");
-         screen.displayMessageLine("4 - $2,000");
-         screen.displayMessageLine("5 - $5,000");
-         screen.displayMessageLine("6 - Other amount");
-         screen.displayMessageLine("7 - Cancel transaction");
-         screen.displayMessage("\nChoose a withdrawal option: ");
+         screen.displayMessageLine( "\nWithdrawal Menu:" );
+         screen.displayMessageLine( "1 - $20" );
+         screen.displayMessageLine( "2 - $40" );
+         screen.displayMessageLine( "3 - $60" );
+         screen.displayMessageLine( "4 - $100" );
+         screen.displayMessageLine( "5 - $200" );
+         screen.displayMessageLine( "6 - Cancel transaction" );
+         screen.displayMessage( "\nChoose a withdrawal amount: " );
 
          int input = keypad.getInput(); // get user input through keypad
 
          // determine how to proceed based on the input value
-         switch (input) {
-            case 1: // if the user chose a withdrawal amount
+         switch ( input )
+         {
+            case 1: // if the user chose a withdrawal amount 
             case 2: // (i.e., chose option 1, 2, 3, 4 or 5), return the
             case 3: // corresponding amount from amounts array
             case 4:
             case 5:
-               userChoice = amounts[input]; // save user's choice
-               break;
-            case CUSTOM_AMOUNT: // user wants to enter a custom amount
-               userChoice = promptForCustomAmount();
-               break;
+               userChoice = amounts[ input ]; // save user's choice
+               break;       
             case CANCELED: // the user chose to cancel
                userChoice = CANCELED; // save user's choice
                break;
-            default: // the user did not enter a value from 1-7
-               screen.displayMessageLine(
-                     "\nInvalid selection. Try again.");
+            default: // the user did not enter a value from 1-6
+               screen.displayMessageLine( 
+                  "\nIvalid selection. Try again." );
          } // end switch
       } // end while
 
       return userChoice; // return withdrawal amount or CANCELED
    } // end method displayMenuOfAmounts
-
-   // prompt user to enter a custom withdrawal amount
-   private int promptForCustomAmount() {
-      Screen screen = getScreen(); // get screen reference
-      int amount = 0;
-
-      while (amount == 0) {
-         screen.displayMessage("\nPlease enter a withdrawal amount" +
-               "\n(multiples of 100, 500, or 1000 only): HK$");
-         int input = keypad.getInput();
-
-         // check if amount is valid (multiple of 100, 500, or 1000)
-         if (input == CANCELED)
-            return CANCELED;
-         else if (input % 100 == 0 && input > 0) {
-            // amount is valid if it's a multiple of any of the allowed values
-            amount = input;
-         } else {
-            screen.displayMessageLine(
-                  "\nInvalid amount. Please enter a multiple of 100, 500, or 1000.");
-         }
-      }
-
-      return amount;
-   } // end method displayMenuOfAmounts
 } // end class Withdrawal
 
+
+
 /**************************************************************************
- * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and *
- * Pearson Education, Inc. All Rights Reserved. *
- * *
- * DISCLAIMER: The authors and publisher of this book have used their *
- * best efforts in preparing the book. These efforts include the *
- * development, research, and testing of the theories and programs *
- * to determine their effectiveness. The authors and publisher make *
- * no warranty of any kind, expressed or implied, with regard to these *
+ * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
  * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or *
- * consequential damages in connection with, or arising out of, the *
- * furnishing, performance, or use of these programs. *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
  *************************************************************************/
